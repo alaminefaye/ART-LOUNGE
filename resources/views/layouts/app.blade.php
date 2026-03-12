717637016789271d4ace901d71a017e6cb1e7c06
Al AMINE FAYE <Zhuanz@192.168.1.9>
2026-03-12 04:25:45 +0000

    correction

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,9 +41,8 @@
             <!-- Menu -->
             <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
                 <div class="app-brand demo">
-                    <a href="{{ route('dashboard') }}" class="app-brand-link d-flex align-items-center gap-2">
+                    <a href="{{ route('dashboard') }}" class="app-brand-link d-flex align-items-center">
                         <img src="{{ asset('assets/img/logo.png') }}" alt="Logo" style="height: 64px; width: auto;" />
-                        <span class="app-brand-text demo menu-text fw-bolder">{{ config('app.name', 'Dolce Vita') }}</span>
                     </a>
                     <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
                         <i class="bx bx-chevron-left bx-sm align-middle"></i>
